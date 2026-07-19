21cc3ef265f465864499ab70915a99de250e1869
DONGTIEN17 <dongtiennguyen17@gmail.com>
2026-07-19 16:39:32 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dongtien17.github.io',
   base: '/Travel_Blog',
+  integrations: [mdx(), sitemap()],
 });
